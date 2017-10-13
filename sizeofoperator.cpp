d0d44299b9014cdd0a10dee0ad094984d68a7dbe
shivaji1000 <prabhatsharma.prabhat00@gmail.com>
2017-10-13 12:14:58 -0700

    Add files via upload

---
+++ sizeofoperator.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+#include<math.h>
+using namespace std;
+int main()
+{int a,b,c,d,e;
+ int sum=0;
+    cin>>a;
+    sum +=a;
+    cout<<sum;
+    cin>>b;
+    sum +=b;
+    cout<<sum;
+    cin>>c;
+    sum +=c;
+    cout<<sum;
+    cin>>d;
+    sum +=d;
+    cout<<sum;
+
+}
